e35ccc0f5d6ad877fb0d5204adea16474f3d556f
two135 <two135@uw.edu>
2022-10-02 22:32:50 -0700

    push

---
+++ wasd_movement_2d/Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode jumpKey;
+
+    public float speed;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        // "w" can be replaced with any key
+        // this section moves the character up
+        if (Input.GetKey("w"))
+        {
+            speed = 10f;
+            pos.y += speed * Time.deltaTime;
+        }
+
+        // "s" can be replaced with any key
+        // this section moves the character down
+        if (Input.GetKey("s"))
+        {
+            speed = 5f;
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        // "d" can be replaced with any key
+        // this section moves the character right
+        if (Input.GetKey("d"))
+        {
+            speed = 20f;
+            pos.x += speed * Time.deltaTime;
+        }
+
+        // "a" can be replaced with any key
+        // this section moves the character left
+        if (Input.GetKey("a"))
+        {
+            speed = 20f;
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
